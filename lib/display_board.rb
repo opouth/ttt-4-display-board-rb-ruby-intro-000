117e9ea42ab64c99c670ebeecf084edbf8bc4c65
Ermite Rikudo <opouth@hotmail.fr>
2020-05-11 16:51:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
+board = Array.new(9, "   ")
+def display_board(board)
+  9.times do |a|
+    if a%3 != 0
+      print " "
+    else
+      print 11.times "-"
+    end
+    a +=1
+  end
+  puts board
+end
+display_board(board)
 # out the current state.
