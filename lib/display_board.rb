1ae8cace8c58f6fef855053c4c291cf5c5af3428
Ermite Rikudo <opouth@hotmail.fr>
2020-05-11 16:56:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,12 @@
 # Define display_board that accepts a board and prints
 board = Array.new(9, "   ")
 def display_board(board)
-  
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
   puts board
 end
 display_board(board)
